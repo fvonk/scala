5756a4573ae0704c936a593758530460de532281
Pavel Kozlov <pavelcauselov@gmail.com>
2020-08-24 12:56:13 +0300

    fuck

--- hello-world/src/main/scala/Ch3.scala
+++ hello-world/src/main/scala/Ch3.scala
@@ -4,8 +4,14 @@ object Ex1 extends App {
     def loop(n: Int): Array[Int] = (for (i <- 0.until(n)) yield i).toArray
 }
 
-object Ex1 extends App {
-    println(loop(5).mkString)
+// object Ex2 extends App {
+//     println(flip(Array(1, 2, 3, 4, 5)))
 
-    def loop(n: Int): Array[Int] = (for (i <- 0.until(n)) yield i).toArray
-}
+//     def flip(arr: Array[Int]): Array[Int] = {
+//         val end = arr.length % 2 == 0 ? arr.length : arr.length - 1
+//         for (i <- 0.to(end)) {
+//             // arr(i) 
+//         }
+//         arr
+//     }
+// }
